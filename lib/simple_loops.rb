f8ad2ac313bc432979ca7f072283a1cc2e20b30a
Junko Tahara <junkotahara911@gmail.com>
2020-05-25 16:42:52 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, num)
+  num.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  array.each { |word| puts word }  
+end
+
+def return_string_array(array)
+  array.map { |item| item.to_s }
+end
